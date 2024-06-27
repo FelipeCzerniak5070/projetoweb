4400db72baf6ebc4e6182f6a4ec37d57948f1f45
Felipe Czerniak <felipeczerniak@gmail.com>
2024-06-27 20:45:40 -0300

    Adição do Formulário

--- back-end/index.php
+++ back-end/index.php
@@ -1,2 +1,6 @@
+
+
 <?php
-    echo "Olá, UDESC!";
+    echo ("<h1>Nome: ".$_POST['nome']."</h1>");
+    echo ("<h1>Idade: ".$_POST['idade']."</h1>");
+    //echo '<img src="https://lh6.googleusercontent.com/proxy/KkmqQryciWg2LFCzfFFvYz0O_TxyiYQKUHouMlAC6dLPwVmrnTt_3CImtS-2jGlJMbHfbdHT-LYvj7udITgdUBombpjPJOMGpxWsEysRY2VJDXMLVg" alt="UDESC">';
