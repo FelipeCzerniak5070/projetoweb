6563f1bf35e2f6fd00c11a6d77c91bef2be6dd4b
Felipe Czerniak <felipeczerniak@gmail.com>
2024-06-27 21:16:29 -0300

    Adição no Banco de Dados

--- back-end/index.php
+++ back-end/index.php
@@ -1,6 +1,37 @@
-
-
 <?php
-    echo ("<h1>Nome: ".$_POST['nome']."</h1>");
-    echo ("<h1>Idade: ".$_POST['idade']."</h1>");
+
+$servername = "localhost";
+$username = "root";
+$passwd = "";
+$dbname = "usuario";
+
+$usuario = $_POST['usuario'];
+$email = $_POST['email'];
+$senha = $_POST['senha'];
+
+$connection = new mysqli($servername,$username,$passwd,$dbname);
+
+if($connection->connect_error) {
+    die("Connection failed: " 
+        . $conn->connect_error);
+}
+
+$sql = "INSERT INTO usuario(nome, email, senha) VALUES ('$usuario','$email','$senha')";
+
+if($connection->query($sql)===TRUE) {
+    echo "Sucesso";
+} else {
+    echo "Erro: " . $sql . "<br>" . $connection->error;
+}
+
+
+    echo '
+    <img height="100" width="140" src="https://lh6.googleusercontent.com/proxy/KkmqQryciWg2LFCzfFFvYz0O_TxyiYQKUHouMlAC6dLPwVmrnTt_3CImtS-2jGlJMbHfbdHT-LYvj7udITgdUBombpjPJOMGpxWsEysRY2VJDXMLVg" alt="UDESC">
+    ';
+
+
+
+    echo ("<h1>Nome: ".$_POST['usuario']."</h1>");
+    echo ("<h1>E-mail: ".$_POST['email']."</h1>");
+    echo ("<h1>Senha: ".$_POST['senha']."</h1>");
     //echo '<img src="https://lh6.googleusercontent.com/proxy/KkmqQryciWg2LFCzfFFvYz0O_TxyiYQKUHouMlAC6dLPwVmrnTt_3CImtS-2jGlJMbHfbdHT-LYvj7udITgdUBombpjPJOMGpxWsEysRY2VJDXMLVg" alt="UDESC">';
